a909e7b58f5317e819466060c603224c1df2da78
Tilak Popat <tilakpopat2007@gmail.com>
2026-05-14 16:01:45 +0530

    fix: increase image upload max size to 128MB to support large original files

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,7 +6,7 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "16MB" } })
+  imageUploader: f({ image: { maxFileSize: "128MB" } })
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("Upload complete for image");
       console.log("file url", file.url);
